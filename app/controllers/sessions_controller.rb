c540f94af52ba074b3e8d4ab10a5c6bce259870f
wlytle <wlytle89@gmail.com>
2020-11-07 13:51:37 -0700

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to :root
+  end
+end
